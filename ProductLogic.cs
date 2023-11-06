cc6314c1ceb7b2307094d79c74991e43a1822a8a
Melody Miller <8bitmel0dee@gmail.com>
2023-11-06 16:52:51 -0500

    testing connection between git and github after resetting pc

---
+++ ProductLogic.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExercise1
+{
+
+    public class ProductLogic
+        {
+            // Private list of products
+            private List<Product> _products;
+
+            // Private dictionary - dog leashes
+            private Dictionary<string, DogLeash> _dogLeashes;
+
+            // Private dictionary - cat food
+            private Dictionary<string, CatFood> _catFoods;
+
+
+            // Constructor
+            public ProductLogic()
+            {
+                // Instance List - Products
+                _products = new List<Product>();
+
+                // Instance Dictionary - DogLeashes
+                _dogLeashes = new Dictionary<string, DogLeash>();
+
+                // Instance Dictionary - CatFood
+                _catFoods = new Dictionary<string, CatFood>();
+            }
+
+            // AddProduct method
+            public void AddProduct(Product product)
+            {
+                // Check if the product is a dog leash
+
+                if (product is DogLeash)
+                {
+                    // Convert product
+                    DogLeash dogLeash = product as DogLeash;
+
+                    // Add the dog leash to the dictionary
+                    _dogLeashes.Add(dogLeash.Name, dogLeash);
+                }
+
+                else if (product is CatFood)
+                {
+                    CatFood catFood = product as CatFood;
+
+                    _catFoods.Add(catFood.Name, catFood);
+                }
+                else
+                {
+                    _products.Add(product);
+
+                }
+
+            }
+
+            // GetAll Method that returns the products
+            public List<Product> GetAllProducts()
+            {
+                return _products;
+            }
+
+            //
+            public DogLeash GetDogLeashByName(string name)
+            {
+                if (_dogLeashes.ContainsKey(name))
+                {
+                    return _dogLeashes[name];
+                }
+                else
+                {
+                    return null;
+                }
+
+
+            }
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿using ClassExercise1;
+using System.Text.Json;
+
+
+
+var productLogic = new ProductLogic(); //instance
+
+Console.WriteLine("Press 1 to add a cat food product");
+Console.WriteLine("Press 2 to add a dog leash");
+Console.WriteLine("Type 'exit' to quit");
+
+string userInput = Console.ReadLine();
+
+CatFood catFood = new CatFood();
+
+DogLeash dogLeash = new DogLeash();
+
+
+
+
+while (userInput.ToLower() != "exit")
+{
+    if (userInput == "1")
+    {
+
+        Console.Write("Enter cat food name: ");
+        string name = Console.ReadLine();
+        catFood.Name = name;
+
+        Console.Write("Enter the price: ");
+
+        decimal price = decimal.Parse(Console.ReadLine());
+        catFood.Price = price;
+
+        Console.Write("Enter the quantity: ");
+
+        int quantity = int.Parse(Console.ReadLine());
+        catFood.Quantity = quantity;
+
+        Console.Write("Enter the description: ");
+
+        string description = Console.ReadLine();
+        catFood.Description = description;
+
+        Console.Write("Enter the weight in pounds: ");
+
+        double weight = double.Parse(Console.ReadLine());
+        catFood.WeightPounds = weight;
+
+        Console.Write("Is it kitten food? (yes/no): ");
+
+        string kitten = Console.ReadLine();
+        bool isKitten = kitten == "yes";
+        catFood.KittenFood = isKitten;
+
+        Console.WriteLine();
+       
+        productLogic.AddProduct(catFood);
+
+        Console.WriteLine("Success!");
+       
+
+        
+
+
+    }
+    else if (userInput == "2") 
+    {
+
+        Console.Write("Enter the dog leash name: ");
+        string name = Console.ReadLine();
+        dogLeash.Name = name;
+
+        Console.Write("Enter the price: ");
+
+        decimal price = decimal.Parse(Console.ReadLine());
+        dogLeash.Price = price;
+
+        Console.Write("Enter the quantity: ");
+
+        int quantity = int.Parse(Console.ReadLine());
+        dogLeash.Quantity = quantity;
+
+        Console.Write("Enter the description: ");
+
+        string description = Console.ReadLine();
+        dogLeash.Description = description;
+
+        Console.Write("Enter the length in inches: ");
+
+        int length = int.Parse(Console.ReadLine());
+        dogLeash.LengthInches = length;
+
+        Console.Write("What kind of material?");
+
+        string material = Console.ReadLine();
+
+        dogLeash.Material = material;
+
+        Console.WriteLine();
+      
+        productLogic.AddProduct(dogLeash);
+        
+        Console.WriteLine("Success!");
+
+        
+     
+
+
+    }
+
+    
+    //testing git connection to github after resetting PC 
+
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
